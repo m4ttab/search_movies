cda8391bc375386888ab0c1369261397c1bcc33a
Mateusz Taboran <m4ttab@gmail.com>
2016-11-06 14:10:33 +0100

    edited comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,9 @@
 class CommentsController < 	ApplicationController
+before_action :authenticate_user!
 
 def create
 	@comment = @commentable.comments.new(comment_params)
+	@comment.user_id = current_user.id
 	@comment.save
 	redirect_to @commentable, notice: "Your comment was added"
 end
@@ -10,7 +12,7 @@ end
 private
 
 def comment_params
-	params.require(:comment).permit(:body)
+	params.require(:comment).permit(:body, :user)
 end
 
 
